45576b83c78c3bdccaf9fc589a9ad8aaac069ce8
Gustavo Bianco <gustavobianco277@gmail.com>
2025-05-23 12:53:49 -0300

    initil commit

---
+++ src/main/java/com/farmacia/repository/VendaRepository.java
@@ -0,0 +1,4 @@
+package com.farmacia.repository;
+
+public class VendaRepository {
+}
--- src/test/java/com/farmacia/FarmaciaApplicationTests.java
+++ src/test/java/com/farmacia/FarmaciaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.farmacia.farmacia;
+package com.farmacia;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
