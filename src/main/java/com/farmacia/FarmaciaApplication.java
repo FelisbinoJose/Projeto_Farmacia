a631884fd0b30fb84e398743393049329c78c659
FelisbinoJose <246760.jose@unibave.net>
2025-10-16 23:16:51 -0300

    feat: frontend pronto

--- src/main/java/com/farmacia/FarmaciaApplication.java
+++ src/main/java/com/farmacia/FarmaciaApplication.java
@@ -8,7 +8,6 @@ public class FarmaciaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FarmaciaApplication.class, args);
-		System.out.println("Farmacia Application Started");
 	}
 
 }
